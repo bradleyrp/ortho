c0613f9fea93244a9661d89cba8b5e12019a88b8
Ryan Bradley <bradleyrp@gmail.com>
2021-09-15 12:24:42 -0400

    added locker functions to the root level

--- ortho/__init__.py
+++ ortho/__init__.py
@@ -5,10 +5,13 @@ __all__ = [
 	'version',
 	'ortho_interact',
 	'print_function','ortho_print',
-	'ortho_debugger','ortho_debugger_click',]
+	'ortho_debugger','ortho_debugger_click',
+	'state_user','SimpleFlock']
 
 from ._version import version
 from .reexec import interact as ortho_interact
 from .reexec import debugger as ortho_debugger
 from .reexec import debugger_click as ortho_debugger_click
 from .logs import stylized_print as ortho_print
+from .locker import state_user
+from .locker import SimpleFlock
